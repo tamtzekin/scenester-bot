32d82a00950dde04dd4914165a706d1071f84822
Justin Tam <tamtzekin@gmail.com>
2017-04-26 00:41:08 +1000

    Added emojis to bot responses

--- README.md
+++ README.md
@@ -1,30 +1,34 @@
 # sohwhatson
 
+# Author
+Justin Tam (vkimmp3)
+
 ## Description
 Recommends to users shows and events at the Sydney Opera House based on their custom inputs of interests and moods.
 Dialogue tree guides user organically through commands.
 
 ## Commands
-"Hi" - introduces user to the bot
-"What's on today?" - open up options for different shows on
-"Show me something cool" - bot pulls a random selection from the shows marked as "cool"
-"Show me something fancy" - bot pulls a random selection from the shows marked as "fancy"
-"Show me something stimulating" - bot pulls a random selection from the shows marked as "stimulating"
+- "Hi" - introduces user to the bot
+- "What's on today?" - open up options for different shows on
+- "Show me something cool" - bot pulls a random selection from the shows marked as "cool"
+- "Show me something fancy" - bot pulls a random selection from the shows marked as "fancy"
+- "Show me something stimulating" - bot pulls a random selection from the shows marked as "stimulating"
+- "Show me something pretty" - (Easter egg) Bot posts an random Instagram photo (selfie) of the Opera House looking good
 
 ## Approach
-1. Pseudocode written outlining possible conversation trees. N.B. Commands can be used at any time in the chat, however dialogue organically guides the user through useable commands.
-2. Key event details from sydneyoperahouse.com used to determine the pool of shows which a user can choose from.
-3. Created first greeting dialogue for bot.
-4. Created arrays for each 'genre' (music, talks, etc.). Filled these arrays with three key shows.
-5. Wrote bot.respond to cycle through conditionals based on what genre of show the user inputs (wth the "I like ..." command).
-6. Hubot commands can be quite direct, so used more open questions to give bot a more conversational tone and appearance (eg. "Show me something cool / fancy / stimulating" commands)
-7. Added Instagram and YouTube links to recommended shows.
-
+1.  Pseudocode written outlining possible conversation trees. N.B. Commands can be used at any time in the chat, however dialogue organically guides the user through useable commands.
+2.  Key event details from sydneyoperahouse.com used to determine the pool of shows which a user can choose from.
+3.  Created first greeting dialogue for bot.
+4.  Created arrays for each 'genre' (music, talks, etc.). Filled these arrays with three key shows.
+5.  Wrote bot.respond to cycle through conditionals based on what genre of show the user inputs (wth the "I like ..." command).
+6.  Hubot commands can be quite direct, so used more open questions to give bot a more conversational tone and appearance (eg. "Show me something cool / fancy / stimulating" commands)
+7.  Added Instagram and YouTube links to recommended shows.
 
 ## Installation
-1.
-2.
-
+1.  Once you have Hubot installed, navigate to your Hubot projects directory.
+2.  Edit `hubot.scripts.json` and add 'sohwhatson.js' to the array.
+3.  Alternatively, copy files from this repository into your `scripts` directory.
+4.  Restart your Hubot.
 
 ## Challenges / Next steps
 - Code originally based on genre types ("contemporary music" "classical music" "talks" "conferences"). This was difficult to contain based on the amount of variables (ie. a user could say "I want to see baroque music"), so this was instead changed to "moodType". This allowed for three categories ("cool" | "fancy" | "stimulating"), which also allowed for breadth of show recommendations beyond genre.
--- scripts/script_current.js
+++ scripts/script_current.js
@@ -1,3 +1,4 @@
+// sohwhatson
 // Bot that recommends shows and events at the Sydney Opera House to users based on their mood inputs.
 
 module.exports = (bot) => {
@@ -5,33 +6,44 @@ module.exports = (bot) => {
 // SOH Bot intros itself and gives dialogue prompts.
   bot.hear(/Hi/, res => { // capital variants here too
     res.reply("Let me help you find something to do here. Just ask me, 'what's on today'?")
-    res.reply("https://www.instagram.com/p/BTANl6Jh4yj/?taken-by=sydneyoperahouse")
-    return res.reply("Hey! Welcome to the Sydney Opera House. It's a beautiful day down here...")
+    res.reply("https://www.instagram.com/p/BPGPLUtgTkQ/")
+    return res.reply("Hey! Welcome to the Sydney Opera House. It's a beautiful day down here :sun_with_face::sun_with_face:")
 });
 
   bot.hear(/What's on today?|what's on today?|whats on today|whats on today?/, res => { // when i add more variants, it loops my code
-    res.reply("What are you looking for? Let me help you pick. (Try typing, '@sohwhatson Show me something cool' / 'Show me something fancy' / 'Show me something stimulating').")
-    return res.reply("There's so much on! From the classics like ballet, to guitar-playing swooners.") // put real date here?
+    res.reply("Let me help you pick! Do you want to see something COOL:dark_sunglasses:? Or something FANCY:wine_glass:? Or do you want something STIMULATING:studio_microphone:? (Try typing, '@sohwhatson Show me something cool').")
+    return res.reply("There's so much on! From the classics like ballet:two_hearts:, to a few classic rock gods:guitar:, to the great thinkers of our time:books:.") // put real date here?
 });
 
 // Defines arrays for different kinds of shows, arranged by genre or "mood".
 const coolShows = [
-  "We don't just do orchestras in here. The rave godfathers Underworld are playing tonight!" + "\nhttps://www.youtube.com/watch?v=krN3ledny-Y",
-  "Billy Bragg & Joe Henry are serenading together in the Concert Hall this weekend. Proper political balladeers. " + "\nhttps://www.youtube.com/watch?v=mgwU4zCEJtY",
-  "Mary J. Blige is on this Tuesday. The reigning queen of soul! " + "\nhttps://www.youtube.com/watch?v=BgcSHzgZ8gQ", // date night tag
-  "The Sydney Symphony Orchestra are performing the score live to the first Harry Potter movie. Haven't seen that one for years! " + "\nhttps://www.youtube.com/watch?v=YOVS9yn2R7c"
+  "We don't just do orchestras in here. The rave godfathers Underworld are playing tonight!:loud_sound::loud_sound:" + "\nhttps://www.youtube.com/watch?v=krN3ledny-Y",
+  "Billy Bragg & Joe Henry :flag-gb: are serenading together in the Concert Hall this weekend. Proper political balladeers. " + "\nhttps://www.youtube.com/watch?v=mgwU4zCEJtY",
+  ":fire::100:Mary J. Blige is on this Tuesday. The reigning queen of soul! " + "\nhttps://www.youtube.com/watch?v=BgcSHzgZ8gQ", // date night tag
+  "The Sydney Symphony Orchestra are performing the score live to the first Harry Potter movie. Haven't seen that one for years! :revolving_hearts:" + "\nhttps://www.youtube.com/watch?v=YOVS9yn2R7c"
 ];
 
 const fancyShows = [
   "Are you into ballet? But you like the edgier stuff, right? You'll enjoy 'Faster'' by The Australian Ballet." + "\nhttps://www.youtube.com/watch?v=W5I28CQBIro",
-  "If you like the classics, you really need to see 'The Nutcracker' ... with an Australian twist. " + "\nhttps://www.instagram.com/p/BS4mmP2F2V0/",
-  "Joshua Bell and Academy of St Martin in the Fields are performing twice this weekend! Beethoven, Mozart, Schumann, Mendelssohn...quite possibly the best orchestra in the world right now. " + "\nhttps://www.youtube.com/watch?v=laGT9IB2bFo"
+  "If you like the classics, you really need to see 'The Nutcracker' ... with an Australian twist. :dancer: " + "\nhttps://www.instagram.com/p/BS4mmP2F2V0/",
+  "Joshua Bell and Academy of St Martin in the Fields :violin: are performing twice this weekend! Beethoven, Mozart, Schumann, Mendelssohn...quite possibly the best orchestra in the world right now. " + "\nhttps://www.youtube.com/watch?v=laGT9IB2bFo",
+  "You know we do food too? Celebrate Mother's Day in style :cocktail: with Australia's best pavlova. You can almost taste it from here... " + "\nhttps://www.instagram.com/p/BQE-eyhjqak/"
 ];
 
 const stimulatingShows = [
-  "Singer and poet legend Patti Smith is doing a talk in the Concert Hall this Sunday. It'll challenge you, I'm sure. " + "\nhttps://www.instagram.com/p/4mYawpRc6T/",
-  "Djuki Mala might be your thing. Think contemporary dance fused with traditional Indigenous movement. " + "\nhttps://www.instagram.com/p/BTOWSi9FdkK/",
-  "We're proud to present 'The 7 Stages of Grieving', an Aboriginal classic which confronts the grief of Indigenous people and their hopes for reconciliation. " + "\nhttps://www.youtube.com/watch?v=zbPuSX2wiC4"
+  "Singer, poet and :guitar: goddess Patti Smith is talking about her life and music in the Concert Hall this Sunday. " + "\nhttps://www.instagram.com/p/4mYawpRc6T/",
+  "Djuki Mala might be your thing. Think contemporary dance fused with traditional Indigenous movement.:muscle: " + "\nhttps://www.instagram.com/p/BTOWSi9FdkK/",
+  "We're proud to present 'The 7 Stages of Grieving', an Aboriginal classic which confronts the grief of Indigenous people and their hopes for reconciliation.:yellow_heart: " + "\nhttps://www.youtube.com/watch?v=zbPuSX2wiC4",
+  "Dig into the deep and dirty world of Aussie talkback radio :radio: with Jonathan Biggins' witty theatre production 'Talk'.:studio_microphone: " + "\nhttps://www.youtube.com/watch?v=taoTRFsLOr4"
+];
+
+const prettyPicture = [
+  "\nhttps://www.instagram.com/p/BQzd8bPBgFZ/",
+  "\nhttps://www.instagram.com/p/BSNrTHohr16/",
+  "\nhttps://www.instagram.com/p/BSzvH2-Bltm/",
+  "\nhttps://www.instagram.com/p/BTANl6Jh4yj/",
+  "\nhttps://www.instagram.com/p/BQPYEo0AkT5/",
+  "\nhttps://www.instagram.com/p/BMgCQ5ChGNz/"
 ];
 
 // User picks the type ("mood") of show they want to see and bot gives randomised suggestions based on moodType arrays.
@@ -39,8 +51,8 @@ const stimulatingShows = [
     const moodType = res.match[1];
 
     console.log(moodType)
-    if (moodType === "music") {
-      return res.send("Music? There's a lot of music out there. You're going to have to be a bit more specific. If you want something edgy, try typing '@sohwhatson Show me something cool'")
+    if (moodType === "pretty") {
+      return res.send("Oh, yeah? You're looking right at it :fire: :kiss: :wink: " + res.random(prettyPicture));
     } else if (moodType === "cool") {
       return res.send(res.random(coolShows));
     } else if (moodType === "fancy") {
@@ -48,24 +60,11 @@ const stimulatingShows = [
     } else if (moodType === "stimulating") {
       return res.send(res.random(stimulatingShows));
     } else {
-      return res.reply(`${moodType}? I don't think we have anything ${moodType} on today. Try typing '@sohwhatson Show me something fancy'`)
+      return res.reply(`${moodType}? I don't know about that ... you're being a bit broad. You know this is the Opera House, right? Try typing '@sohwhatson Show me something fancy'.`)
     }
   });
 }
 
-// //
-// // // for number of tickets
-// // bot.respond(/(.*) tickets/i, res => {
-// //   // parseInt() turns a string into an integer
-// //   const a = parseInt(res.match[1])
-// //   const b = parseInt(res.match[2])
-// //
-// //   const sum = a + b
-// //
-// //   return res.reply(`${a} + ${b} = ${sum}`)
-// // });
-
-//
 // //time out command
 // //Are you still there? Just tell me "I want to see" followed by the type of show.
 // // You could say, "I want to see family shows," or "I want to see contemporary music".
